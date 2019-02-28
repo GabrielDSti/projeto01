85c8da8a74bc3b58d675975f36400dba38be943c
GabrielDSti <gabriel.sistemasti@gmail.com>
2019-02-28 00:00:52 -0300

    Classes e Dao's

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,74 @@
+public class Cliente {
+    private String nomeCliente;
+    private String telefone;
+    private String rua;
+    private String setor;
+    private Integer quadra;
+    private Integer lote;
+    private String referencia;
+    private Long id;
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public Integer getQuadra() {
+        return quadra;
+    }
+
+    public void setQuadra(Integer quadra) {
+        this.quadra = quadra;
+    }
+
+    public Integer getLote() {
+        return lote;
+    }
+
+    public void setLote(Integer lote) {
+        this.lote = lote;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/PedidoProduto.java
@@ -0,0 +1,2 @@
+public class PedidoProduto {
+}
